ade588b745e152385bf78fd2eedb334ffaba0ef8
lupeng@iMac <pengloo53@gmail.com>
2021-06-12 22:05:25 +0800

    优化部分样式

--- public/lupeng-content.md
+++ public/lupeng-content.md
@@ -1,10 +1,29 @@
 它是基于 [wechat-format](https://github.com/lyricat/wechat-format) 项目做的一个服务端版本，目的是为了提供一些 API 功能。
 
-目前暂未实现相关功能，项目开源地址： [wechat-format-server](https://github.com/pengloo53/wechat-format-server)
+目前暂未实现相关功能，项目开源地址： [wechat-format-server](https://github.com/pengloo53/wechat-format-server)，项目可能搁浅了，目前留给自己使用了。
 
----
-![](http://img.sher.ren/weixin/qrcode.jpg)
+## 一些样式
+这是 `code` 的样式。
 
-题图：From Zoommy  
+这是代码块样式：
+
+```js
+var hello = 'world'
+console.log('hello' + hello)
+```
+
+这是引用样式：
+> 知识不是力量，知识分享才是力量。
+
+这是图片样式：
+
+![](http://img.90byte.com/weixin-qrcode.jpg)
+
+题图：From Zoommy   
 推荐阅读：
+
+1. 《把自己作为方法》
+2. 《利息理论》
+3. 《最好的告别》
+4. 《影响力》
 
--- public/themes/lupeng.js
+++ public/themes/lupeng.js
@@ -1,8 +1,9 @@
 module.exports = {
     BASE: {
         'text-align': 'justify',
-        'line-height': '1.75em',
-        'letter-spacing': '0.05em',
+        'line-height': '1.7em',
+        'min-height': '1em'
+        // 'letter-spacing': '0.05em',
     },
     block: {
         h2: {
@@ -10,16 +11,17 @@ module.exports = {
             'text-align': 'center',
             'font-weight': 'normal',
             'text-align': 'center',
-            'margin': '60px 10px 30px 10px'
+            'margin': '60px 10px 30px 0px'
         },
         h3: {
             'font-weight': 'bold',
             'font-size': '120%',
-            'margin': '40px 10px 20px 10px'
+            'margin': '40px 10px 20px 0px'
         },
         p: {
-            'margin': '0 10px 20px 10px',
+            'margin': '0 0 1em 0',
             // 'line-height': '1.75'
+
         },
         blockquote: {
             'color': '#9a9a9a',
@@ -34,16 +36,20 @@ module.exports = {
             'overflow': 'auto',
             'background': 'rgb(247, 247, 247)',
             'border-radius': '2px',
-            'padding': '10px',
+            'padding': '1px 3px',
             'line-height': '1.3',
-            'border': '1px solid rgb(236,236,236)',
-            'margin': '20px 0',
+            // 'border': '1px solid #66b1ff',
+            'margin': '10px 0',
+            'color': '#3a8ee6'
         },
         image: {
             'border-radius': '4px',
             'display': 'block',
             'margin': '20px auto',
-            'width': '100%',
+            'max-width': '100%',
+            // 'box-shadow': '5px 5px 10px 1px #ccc',
+            // '-moz-box-shadow': '5px 5px 10px 1px #ccc',
+            // '-webkit-box-shadow': '5px 5px 10px 1px #ccc'
         },
         image_org: {
             'border-radius': '4px',
@@ -73,15 +79,16 @@ module.exports = {
         },
         codespan: {
             'font-size': '0.8em',
-            'color': '#d14',
+            'color': '#3a8ee6',
             'background': '#fefefe',
-            'padding': '3px 5px 0px',
-            'border': '1px solid #ddd',
+            'padding': '1px 3px',
+            'border': '1px solid #66b1ff',
             'margin': '0px 2px',
-            'border-radius': '3px',
+            'border-radius': '2px',
         },
         link: {
-            'color': '#ff3502'
+            'color': '#ff3502',
+            'line-height': '1em'
         },
         wx_link: {
             'color': '#576b95',
